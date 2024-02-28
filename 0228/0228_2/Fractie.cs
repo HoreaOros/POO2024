721e6648e59163c50141fac7dd5254cea588cc17
Horea Oros <horea.oros@gmail.com>
2024-02-28 11:56:06 +0200

    add Fractie project

---
+++ 0228/0228_2/Fractie.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection.Metadata;
+
+internal class Fractie
+{
+    private int numarator, numitor;
+
+    // c-tor implicit (default)
+    public Fractie(): this(0, 1)
+    {
+      
+    }
+
+    public Fractie(int numarator): this(numarator, 1)
+    {
+    }
+
+    public Fractie(int numarator, int numitor)
+    {
+        this.numarator = numarator;
+        this.numitor = numitor;
+    }
+
+    public override string ToString()
+    {
+        return $"({numarator} / {numitor})";
+    }
+
+    /// <summary>
+    /// a/b + c / d = 
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public Fractie Add(Fractie other)
+    {
+        Fractie r = new Fractie();
+        r.numarator = this.numarator * other.numitor + this.numitor * other.numarator;
+        r.numitor = this.numitor * other.numitor;
+        return r;
+    }
+    public static Fractie operator+(Fractie lhs, Fractie rhs)
+    {
+        return lhs.Add(rhs);
+    }
+}
---
+++ 0228/0228_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Fractie f1 = new Fractie();
+Console.WriteLine(f1);
+
+Fractie f2 = new Fractie(2, 3);
+Console.WriteLine(f2);
+
+Fractie f3 = new Fractie(5);
+Console.WriteLine(f3);
+
+
+
+Fractie f4 = f2.Add(f3);
+Console.WriteLine(f4);
+
+Fractie f5 = f2 + f3;
+Console.WriteLine(f5);
+
+
+
+
+
