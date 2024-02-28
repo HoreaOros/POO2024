1bb45aa5e3660635167e3c99b3c5eae1d973dfb3
Horea Oros <horea.oros@gmail.com>
2024-02-28 08:58:14 +0200

    add Rational project

---
+++ 0228/0228/Program.cs
@@ -0,0 +1,33 @@
+﻿Rational r1 = new Rational(); // ???
+Rational r2 = new Rational(3, 4);
+Rational r3 = new Rational("2/3");
+Rational r33 = new Rational("2");
+Rational r4 = new Rational(5); // 5/1
+
+
+Console.WriteLine(r3);
+// Adunare
+Rational r5 = r2.Add(r3);
+Rational r6 = r2 + r3; // operator overloading
+
+
+// Scadere
+
+// Inmultire
+
+// Impartire
+
+// Ridicare la putere
+Rational r7 = r2.Power(7);
+r7 = r2 ^ 7;
+
+// Aducere la forma ireductibila
+r7.Ireductibil();
+
+
+// Operatii de egalitate, comparare
+bool b1 = r2 == r3;
+bool b2 = r2 != r3;
+bool b3 = r2 < r3;
+bool b4 = r2 <= r3;
+
---
+++ 0228/0228/Rational.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Rational
+{
+    private int numarator;
+    private int numitor;
+
+    public Rational(): this(0,1)
+    {
+    }
+
+    public Rational(int numarator): this(numarator, 1)
+    {
+    }
+
+    public Rational(string str)
+    {
+        string[] token = str.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+        if (token.Length < 1 || token.Length > 2)
+            throw new ArgumentException("Numar rational invalid");
+
+        if(token.Length == 1)
+        {
+            bool r = int.TryParse(token[0], out numitor);
+            if (r == false)
+                throw new ArgumentException("Numaratorul nu este un numar valid");
+        }
+        else if(token.Length == 2)
+        {
+            bool r1 = int.TryParse(token[0], out numitor);
+            bool r2 = int.TryParse(token[1], out numarator);
+            if (r1 == false || r2 == false)
+                throw new ArgumentException("Numarul Rational nu este valid");
+        }
+    }
+
+    public Rational(int numarator, int numitor)
+    {
+        this.numarator = numarator;
+        this.numitor = numitor;
+    }
+}
