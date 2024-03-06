4a10257b09fda6e29b483cbf534cb22029fd371b
Horea Oros <horea.oros@gmail.com>
2024-03-06 09:14:46 +0200

    add Stack class

---
+++ 0306/0306/MyStack.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+public class MyStack
+{
+    private int[] data;
+    private int capacity = 16;
+    private int size = 0;
+    public MyStack()
+    {
+        Debug.Write($"Creating new stack with capacity {capacity}...");
+        data = new int[capacity];
+        Debug.WriteLine("success!");
+    }
+    public int Pop()
+    {
+        Console.WriteLine("Trying to Pop the stack...");
+        if (size > 0)
+        {
+            size--;
+            if (size == capacity / 4)
+                resize(capacity / 2);
+            Console.WriteLine("success!");
+            return data[size];
+        }
+        else
+            throw new StackEmptyException("A fost apelata functia Pop pe o stiva goala");
+    }
+    public void Push(int value)
+    {
+        Console.WriteLine($"Trying to push {value} into the stack...");
+        if (Count == capacity)
+        {
+            resize(2 * capacity);
+        }
+        data[size++] = value;
+        Console.WriteLine("success!");
+    }
+
+    private void resize(int newCapacity)
+    {
+        Console.Write($"Resizing stack: old capacity = {capacity}, new capacity = {newCapacity}...");
+        int[] newdata = new int[newCapacity];
+        for (int i = 0; i < Count; i++)
+        {
+            newdata[i] = data[i];
+        }
+        capacity = newCapacity;
+        data = newdata;
+        Console.WriteLine("success");
+    }
+
+    public int Peek()
+    {
+        Console.Write("Try to peek into the stack...");
+        if (size > 0)
+        {
+            Console.WriteLine("success");
+            return data[size - 1];
+        }
+        else
+            throw new StackEmptyException("A fost apelata functia Peek pe o stiva goala");
+    }
+    //public int Count 
+    //{ 
+    //    get
+    //    {
+    //        return size;
+    //    }
+    //}
+    public int Count => size;
+}
---
+++ 0306/0306/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Stack<int> st =  new Stack<int>();
+int n = 10;
+for (int i = 0; i < n; i++)
+    st.Push(i);
+
+Console.WriteLine($"Elementul din varful stivei este: {st.Peek()}");
+
+while (st.Count > 0)
+{
+    Console.WriteLine(st.Pop());
+}
+*/
+
+MyStack ms = new MyStack();
+
+ms.Push(5);
+ms.Push(6);
+ms.Push(7);
+
+Console.WriteLine($"Elementul din varful stivei este: {ms.Peek()}");
+
+while(ms.Count > 0)
+{
+    Console.WriteLine(ms.Pop());
+}
+
+
+
