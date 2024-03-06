91aa2e3eeef502ac2d992b0b37120154f15ebe80
Horea Oros <horea.oros@gmail.com>
2024-03-06 12:05:23 +0200

    add MyStack ver 2

---
+++ 0306_2/0306_2/MyStack.cs
@@ -0,0 +1,61 @@
+﻿public class MyStack
+{
+    private int size = 0;
+    private int[] data;
+    private int capacity = 16;
+    public int Count => size;
+    public MyStack()
+    {
+        data = new int[capacity];
+    }
+
+    public void Push(int value)
+    {
+        Console.WriteLine($"Trying to push {value} onto the stack...");
+        if (size == capacity)
+        {
+            resize(2 * capacity);
+        }
+
+        data[size++] = value;
+        Console.WriteLine("Success - push");
+    }
+
+    public int Peek()
+    {
+        if (size > 0)
+            return data[size - 1];
+        else
+            throw new StackEmptyException("Peek(): Stiva este goala");
+    }
+
+    private void resize(int newcapacity)
+    {
+        Console.WriteLine($"Trying to resize stack. Old value {capacity}, New value {newcapacity}");
+        int[] newdata = new int[newcapacity];
+        for(int i = 0; i < size; i++)
+            newdata[i] = data[i];
+        capacity = newcapacity;
+        data = newdata;
+        Console.WriteLine("Success - resize!");
+    }
+
+    public int Pop()
+    {
+        Console.WriteLine("Trying to pop the stack");
+        if (size > 0)
+        {
+            int ret = data[size - 1];
+            size--;
+            if (size == capacity / 4)
+                resize(capacity / 2);
+            Console.WriteLine("Success - pop");
+            return ret;
+        }
+        else
+        {
+            Console.WriteLine("Exception occured. Stack empty!");
+            throw new StackEmptyException("Pop(): stiva este goala");
+        }
+    }
+}
---
+++ 0306_2/0306_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// 1. Utilizarea clasei Stack din biblioteca .NET
+/*Stack<int> stack = new Stack<int>();
+for (int i = 0; i < 10; i++)
+    stack.Push(i);
+Console.WriteLine($"Numarul de elemente din stiva este: {stack.Count}");
+
+while(stack.Count > 0)
+{
+    Console.WriteLine($"{stack.Pop()}");
+}
+
+stack.Pop();
+*/
+
+
+MyStack mystack = new MyStack();
+
+for (int i = 0; i < 100; i++)
+    mystack.Push(i);
+
+Console.WriteLine($"Elementul din varful stivei este: {mystack.Peek()}");
+
+while(mystack.Count > 0)
+{
+    Console.WriteLine(mystack.Pop());
+}
+
+mystack.Pop();
