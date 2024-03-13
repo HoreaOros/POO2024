9478fdd527681f0aaa3d3a1ecd2aea40345b765c
Horea Oros <horea.oros@gmail.com>
2024-03-13 09:18:12 +0200

    add MyDateTime driver

--- 0313/0313/Program.cs
+++ 0313/0313/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
 
+// ***********************************************
+// 1.  DateTime - demo
 DateTime dt = DateTime.Now;
 Console.WriteLine(dt);
 
@@ -20,7 +22,31 @@ Console.WriteLine(dt.Year);
 Console.WriteLine(dt.Hour);
 Console.WriteLine(dt.DayOfWeek);
 Console.WriteLine(dt.DayOfYear);
+// ***********************************************
 
 
 
-MyDateTime mdt = new MyDateTime();
+
+// ***********************************************
+// ***********************************************
+// 2. MyDateTime - demo
+MyDateTime md = new MyDateTime(14, 3, 2023, 8, 59, 42);
+Console.WriteLine(md);
+
+MyDateTime md2 = new MyDateTime(23, 6, 2023);
+MyDateTime md3 = new MyDateTime(an: 2025, luna: 12, zi: 13);
+Console.WriteLine(md3);
+
+if (md2 == md3)
+{
+    Console.WriteLine($"{md2} / {md3} sunt egale");
+}
+else
+{
+    Console.WriteLine($"{md2} / {md3} nu sunt egale");
+}
+
+Console.WriteLine(md2 - md3); // 
+
+
+// ***********************************************
