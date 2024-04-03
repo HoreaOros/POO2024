38be4525792aefe87cdcbdbc9a7b9b4e8c79aa61
Horea Oros <horea.oros@gmail.com>
2024-04-03 12:02:06 +0300

    add Rectangle class

--- 0403/0403_2/Program.cs
+++ 0403/0403_2/Program.cs
@@ -6,3 +6,5 @@ Circle c2 = new Circle { Point = new Point(4, 5), Radius = 10 };
 Console.WriteLine(c);
 Console.WriteLine($"Aria cercului este: {c.Area()}");
 Console.WriteLine($"Lungimea cercului este: {c.Perimeter()}");
+
+Rectangle r = new Rectangle(p, 5, 6);
---
+++ 0403/0403_2/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿internal class Rectangle
+{
+    // fields
+    private Point origin;
+    private int width;
+    private int height;
+
+
+    // c-tor
+    public Rectangle(Point p, int width, int height)
+    {
+        this.origin = p;
+        this.width = width;
+        this.height = height;
+    }
+
+    // properties
+    public Point Point
+    {
+        get
+        {
+            return origin;
+        }
+    }
+    //public int Width
+    //{
+    //    get
+    //    {
+    //        return width;
+    //    }
+    //}
+    //public int Height
+    //{
+    //    get
+    //    {
+    //        return height;
+    //    }
+    //}
+    public int Width => width;
+    public int Height => height;
+}
