8dab79d2e6bc887e930f81b1f7efc6ae97678583
Horea Oros <horea.oros@gmail.com>
2024-04-03 12:59:59 +0300

    create demo code for testing Shape polymorfism

--- 0403/0403_2/Program.cs
+++ 0403/0403_2/Program.cs
@@ -8,3 +8,23 @@ Console.WriteLine($"Aria cercului este: {c.Area()}");
 Console.WriteLine($"Lungimea cercului este: {c.Perimeter()}");
 
 Rectangle r = new Rectangle(p, 5, 6);
+Console.WriteLine(r);
+
+
+List<Shape> shapes = new List<Shape>();
+shapes.Add(c);
+shapes.Add(r);
+shapes.Add(c2);
+
+foreach (Shape shape in shapes)
+    Console.WriteLine(shape);
+
+double totalArea = 0;
+foreach (Shape item in shapes)
+    totalArea += item.Area(); 
+
+//foreach (Shape item in shapes)
+//    if(item is Circle)
+//    {
+//        Circle c3 = (Circle)item;
+//    }
