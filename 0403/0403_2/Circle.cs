6b2e1fde446011ccc5acc5a6fdc8931898426fac
Horea Oros <horea.oros@gmail.com>
2024-04-03 11:49:14 +0300

    add Circle class

---
+++ 0403/0403_2/Circle.cs
@@ -0,0 +1,32 @@
+﻿internal class Circle
+{
+    // properties
+    public Point? Point { get; init; }
+    public int Radius { get; init; }
+
+
+    // c-tors
+    public Circle(Point p, int radius)
+    {
+        this.Point = p;
+        this.Radius = radius;
+    }
+    public Circle()
+    { 
+    }
+
+    // methods
+    public override string ToString()
+    {
+        return $"[Origin: {Point}, Radius = {Radius}]";
+    }
+
+    public double Area()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+    public double Perimeter() // lungimea cercului
+    {
+        return 2 * Math.PI * Radius;
+    }
+}
--- 0403/0403_2/Program.cs
+++ 0403/0403_2/Program.cs
@@ -1,2 +1,8 @@
 ﻿Point p = new Point(2, 3);
 Console.WriteLine(p);
+
+Circle c = new Circle(p, 5);
+Circle c2 = new Circle { Point = new Point(4, 5), Radius = 10 };
+Console.WriteLine(c);
+Console.WriteLine($"Aria cercului este: {c.Area()}");
+Console.WriteLine($"Lungimea cercului este: {c.Perimeter()}");
