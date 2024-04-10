01716f899cfce831f36fc505861e46ad4ab60e43
Horea Oros <horea.oros@gmail.com>
2024-04-10 12:34:48 +0300

    add validation logic for Date; add regions

--- 0410/0410_2/Date.cs
+++ 0410/0410_2/Date.cs
@@ -1,20 +1,55 @@
-﻿using Util;
+﻿using System.Net.Http.Headers;
+using Util;
 internal class Date
 {
-
+    #region Fields
     private int zi;
     private int luna;
     private int an;
+    #endregion
 
+    #region Constructors
     public Date(int zi, int luna, int an)
     {
+        Validate(zi, luna, an);
         this.zi = zi;
         this.luna = luna;
         this.an = an;
+    }
+
+    #endregion
+
+    #region Methods
+    private void Validate(int zi, int luna, int an)
+    {
+        if (an < 0 || an > 9999)
+            throw new ArgumentException("Anul este invalid");
+        if (luna < 1 || luna > 12)
+            throw new ArgumentException("Luna este invalida");
+        var dict = new Dictionary<int, int>
+        {
+            [1] = 31,
+            [2] = 28,
+            [3] = 31,
+            [4] = 30,
+            [5] = 31,
+            [6] = 30,
+            [7] = 31,
+            [8] = 31,
+            [9] = 30,
+            [10] = 31,
+            [11] = 30,
+            [12] = 31,
+        };
+        if ((an % 4 == 0 && an % 100 != 0) || (an % 400 == 0))
+            dict[2]++;
+        if (zi < 1 || zi > dict[luna])
+            throw new ArgumentException("Ziua nu este valida");
     }
     public override string ToString()
     {
         //return $"{zi}-{luna,2:00}-{an}";
         return $"{zi} {luna.ToMonthName()} {an}";
     }
+    #endregion
 }
--- 0410/0410_2/Program.cs
+++ 0410/0410_2/Program.cs
@@ -1,3 +1,14 @@
 ﻿Date d1 = new Date(1, 10, 2000);
 Console.WriteLine(d1);
 
+try
+{
+	Date d2 = new Date(1, 13, 2025);
+	Console.WriteLine(d2);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
